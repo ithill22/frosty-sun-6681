4cc6fcba46d15d0d69573465c1c849d78d06feb6
Isaac Thill <ithill22@turing.edu>
2023-04-24 10:05:21 -0600

    user story 2

--- spec/features/doctors/show_spec.rb
+++ spec/features/doctors/show_spec.rb
@@ -31,5 +31,36 @@ RSpec.describe 'Doctor Show Page' do
         expect(page).to_not have_content(@patient4.name)
       end
     end
+
+    it 'I see a remove button next to each patient' do
+      visit doctor_path(@doctor1)
+
+      within '#patients-list' do
+        expect(page).to have_content(@patient1.name)
+        expect(page).to have_content(@patient2.name)
+        expect(page).to have_content(@patient3.name)
+        within "#patient-#{@patient1.id}" do
+          expect(page).to have_button('Remove')
+        end
+        within "#patient-#{@patient2.id}" do
+          expect(page).to have_button('Remove')
+        end
+        within "#patient-#{@patient3.id}" do
+          expect(page).to have_button('Remove')
+
+          click_button 'Remove'
+        end
+        expect(current_path).to eq(doctor_path(@doctor1))
+        expect(page).to_not have_content(@patient3.name)
+        expect(page).to have_content(@patient1.name)
+        expect(page).to have_content(@patient2.name)
+      end
+
+      visit doctor_path(@doctor3)
+
+      within '#patients-list' do
+        expect(page).to have_content(@patient3.name)
+      end
+    end
   end
 end
